e463d4becc0604f71ac70955f038694f9a5b7295
Cupmouse <cupnmouse@gmail.com>
2018-03-27 11:25:08 +0900

    NewThread view

--- src/ui/view/NewThreadView.jsx
+++ src/ui/view/NewThreadView.jsx
@@ -1,7 +1,89 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { getBoardContractAt } from '../../contract/contract_util';
 
 export default class NewThreadView extends Component {
+  static get propTypes() {
+    return {
+      match: PropTypes.shape({
+        params: PropTypes.shape({
+          boardAddress: PropTypes.string.isRequired,
+        }).isRequired,
+      }).isRequired,
+    };
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.onSubmit.bind(this);
+    this.onTitleChange.bind(this);
+    this.onTextChange.bind(this);
+
+    this.state = {
+      title: '',
+      text: '',
+    };
+  }
+
+  onSubmit(event) {
+    // Stop browser from reloading
+    event.preventDefault();
+
+    // Get contract manipulator
+    const board = getBoardContractAt(this.props.match.params.boardAddress);
+
+    // Get estimation of gas cost
+    board.makeNewThread.estimateGas(this.state.title, this.state.text, (error, gasEstimate) => {
+
+      // Test if it generates error
+      board.makeNewThread.call(this.state.title, this.state.text, {gas: gasEstimate}, (error) => {
+        if (error) {
+          console.error(error);
+          return;
+        }
+
+        // Send actual contract
+        board.makeNewThread.send(this.state.title, this.state.text, (error2, txHash) => {
+          if (error2) {
+            console.error(error2);
+            return;
+          }
+
+          console.log(txHash);
+        });
+      });
+    });
+  }
+
+  onTitleChange(event) {
+    this.setState({ title: event.target.value });
+  }
+
+  onTextChange(event) {
+    this.setState({ text: event.target.value });
+  }
+
   render() {
-    return (<div>yay</div>);
+    return (
+      <div className="content">
+        <form className="new-thread-form" onSubmit={this.onSubmit}>
+          <label className="new-thread-form-label">
+            <span>Title:</span>
+            <input
+              type="text"
+              onChange={this.onTitleChange}
+              value={this.state.title}
+            />
+          </label>
+          <label className="new-thread-form-label">
+            <span>Text:</span>
+            <textarea className="new-thread-form-text" onChange={this.onTextChange} value={this.state.text} />
+          </label>
+          <input id="text-textarea" type="submit" value="Submit" />
+        </form>
+      </div>
+    );
   }
 }
